5dd69ebf9fe7976e099f95a612f8bcb77b380a78
EthanPaleyron <epaleyron@edenschool.fr>
2023-11-12 20:43:53 +0100

    change fini bug image

--- result-change.php
+++ result-change.php
@@ -3,10 +3,15 @@ try {
     $base = new PDO('mysql:host=127.0.0.1;dbname=base1', 'root', '');
     $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = 'UPDATE `posts` SET `Title` =:title, `Comment` =:comment, `File` =:file WHERE Id =:id';
-    $resultat = $base->prepare($sql);
-    $resultat->execute(array("title" => htmlspecialchars($_POST["title"]), "file" => "files/" . $image, "comment" => htmlspecialchars($_POST["comment"]), 'id' => $_GET["id"]));
-    var_dump($resultat);
-    $resultat->closeCursor();
+    $statement = $base->prepare($sql);
+    $statement->execute(array("title" => htmlspecialchars($_POST["title"]), "file" => $image, "comment" => htmlspecialchars($_POST["comment"]), 'id' => $_GET["id"]));
+    // $statement->bindParam(':id', $article["id"]);
+    // $statement->bindParam(':title', $article["title"]);
+    // $statement->bindParam(':file', $article["file"]);
+    // $statement->bindParam(':comment', $article["comment"]);
+    $statement->closeCursor();
+    var_dump($statement);
+    // header("Location: http://localhost/test-pdo/articles.php");
 } catch (Exception $e) {
     throw new InvalidArgumentException($e->getMessage());
 }
